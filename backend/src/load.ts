da8b1228c36bdf0605829bd30ee463dc0bd7ea74
Craig Younkins <cyounkins@gmail.com>
2016-05-05 11:27:09 -0400

    Move from Rethink to Postgres as that is much better suited to this task

--- backend/src/load.ts
+++ backend/src/load.ts
@@ -1,83 +1,34 @@
-import got = require('got');
-import parse = require('csv-parse');
-import r = require('rethinkdb');
+import got      = require('got');
+import parse    = require('csv-parse');
 import highland = require('highland');
+import * as pgPromise from 'pg-promise';
 
-const config = {
-  rethink: {
-    connection_params: {
-      host: '127.0.0.1',
-      port: 28015,
-      db: 'test'
-    },
+var pgp = pgPromise();
 
-    table: 'trips',
-    geo_index: 'path'
-  }
-};
-
+var db = pgp({
+  host: '127.0.0.1', 
+  port: 5432,
+  database: 'uber',
+  user: 'uber',
+  password: 'moo'
+});
 
 function reset_database(): Promise<void> {
   return new Promise<void>(function(resolve, reject) {
-    r.connect(config.rethink.connection_params)
-    .catch(function(err) {
-      if (err) {
-        console.log("Could not open a connection to initialize the database");
-        console.log(err.message);
-        process.exit(1);
-      }
+    db.none("DROP TABLE IF EXISTS trips")
+    .catch(function (error) {
+        console.log("DROP TABLE statement threw error");
+        console.log(error);
     })
-    .then(function(conn) {
-      console.log("Resetting database")
-      r.tableDrop(config.rethink.table).run(conn)
-      .catch(function() {
-        console.log("tableDrop threw error");
-      })
-      .then(function() {
-        console.log("Creating table");
-        return r.tableCreate(config.rethink.table).run(conn);
-      })
-      .catch(function() {
-        console.log("tableCreate threw error");
-      })
-      .then(function() {
-        console.log("ready for insert");
-        conn.close();
-        resolve();
-      })
-    });
-  });
-}
-
-
-function add_index(): Promise<void> {
-  return new Promise<void>(function(resolve, reject) {
-    r.connect(config.rethink.connection_params)
-    .catch(function(err) {
-      if (err) {
-        console.log("Could not open a connection to initialize the database");
-        console.log(err.message);
-        process.exit(1);
-      }
+    .then(function () {
+        return db.none("CREATE TABLE trips (id SERIAL PRIMARY KEY, start_time timestamp, start_point geometry(POINT,4326), end_time timestamp, end_point geometry(POINT,4326), duration_sec int4, path_polyline text)");
     })
-    .then(function(conn) {
-      console.log("Adding index");
-      r.table(config.rethink.table).indexCreate(config.rethink.geo_index, {geo: true}).run(conn)
-      .catch(function() {
-        console.log("indexCreate threw error");
-      })
-      .then(function() {
-        console.log("Waiting for index to finish building");
-        return r.table(config.rethink.table).indexWait(config.rethink.geo_index).run(conn);
-      })
-      .catch(function() {
-        console.log("IndexWait threw error");
-      })
-      .then(function() {
-        console.log("Index created");
-        conn.close();
+    .catch(function (error) {
+        console.log("CREATE TABLE statement threw error");
+        console.log(error);
+    })
+    .then(function () {
         resolve();
-      })
     });
   });
 }
@@ -86,92 +37,103 @@ function add_index(): Promise<void> {
 function load_csv(url:string): Promise<void> {
   return new Promise<void>(function(resolve, reject) {
     console.log("starting to process " + url);
-    r.connect(config.rethink.connection_params)
-    .catch(function(err) {
-      if (err) {
-        console.log("Could not open a connection to initialize the database");
-        console.log(err.message);
+    const parser = parse({delimiter: ','})  
+    .on('error', function(err){
+      console.log(err.message);
+    });
+
+    let total_processed = 0;
+
+    function points_not_same(elements) {
+      if (elements.length < 1) {
+        console.log("err what");
         process.exit(1);
       }
-    })
-    .then(function(conn) {
-      const parser = parse({delimiter: ','})  
-      .on('error', function(err){
-        console.log(err.message);
-      });
-
-      let total_processed = 0;
-
-      function points_not_same(elements) {
-        if (elements.length < 1) {
-          console.log("err what");
-          process.exit(1);
-        }
-        else if (elements.length == 1) {
-          // don't pass
-          return false;
-        }
-        else {
-          const record_start = elements[0];
-          const record_end = elements[1];
-          // pass if start and end locations are not the same
-          return (record_start[2] != record_end[2] || record_start[1] != record_end[1]);
-        }
+      else if (elements.length == 1) {
+        // don't pass
+        return false;
       }
-
-      function convert_pair_to_object(elements) {
-        if (elements.length != 2) {
-          console.log("err what");
-          process.exit(1);
-        }
-
+      else {
         const record_start = elements[0];
         const record_end = elements[1];
+        // pass if start and end locations are not the same
+        return (record_start[2] != record_end[2] || record_start[1] != record_end[1]);
+      }
+    }
 
-        return {
-          date: new Date(Date.parse(record_start[0])),
-          path: r.line([parseFloat(record_start[2]), parseFloat(record_start[1])],
-                       [parseFloat(record_end[2]), parseFloat(record_end[1])]),
-          base: record_start[3]
-        };
+    function convert_pair_to_object(elements) {
+      if (elements.length != 2) {
+        console.log("err what");
+        process.exit(1);
       }
 
-      function insert(err, elements, push, next) {
-        if (err) {
-          // pass errors along the stream and consume next value
-          push(err);
-          next();
-        }
-        else if (elements === highland.nil) {
-          // pass nil (end event) along the stream
-          push(null, highland.nil);
-        }
-        else {
-          total_processed += elements.length;
-          console.log(total_processed);
+      const record_start = elements[0];
+      const record_end = elements[1];
 
-          r.table('trips').insert(elements).run(conn, {durability: "soft"})
-          .then(function() {
-            next();
-          });
-        }
-      }
+      return {
+        start_time: new Date(Date.parse(record_start[0])),
+        start_lat:  parseFloat(record_start[1]),
+        start_lng:  parseFloat(record_start[2]),
+        end_time:   undefined,
+        end_lat:    parseFloat(record_end[1]),
+        end_lng:    parseFloat(record_end[2]),
+        path:       undefined
+      };
+    }
 
-      highland(
+    function add_directions(obj) {
+      return highland(new Promise<any>(function(resolve, reject) {
+        url = "https://api.mapbox.com/v4/directions/mapbox.driving/" + obj.start_lng + ',' + obj.start_lat + 
+          ';' + obj.end_lng + ',' + obj.end_lat + '.json' + '?access_token=pk.eyJ1IjoiY3lvdW5raW5zIiwiYSI6ImNpbnFtcGo5bTEwYTd0cWtqZjJnaGdheGcifQ.IoUfEsKhYimOdVDK3ORcOQ' +
+          '&steps=false&geometry=polyline';
+
+        console.log(url);
+
         got
-          .stream(url)
-          .pipe(parser)
-      ) // convert to highland stream
-        .drop(1)  // drop the header
-        .batch(2) // 2 records at a time
-        .filter(points_not_same) // make sure the start and end are not the same
-        .map(convert_pair_to_object)
-        .batch(200) // N objects to insert at a time
-        .consume(insert)
-        .done(function() {
-          conn.close();
-          resolve();
+        .get(url, {json: true})
+        .then(function(response) {
+          obj.duration_sec = response.body.routes[0].duration;
+          obj.end_time = new Date(obj.start_time.getTime() + obj.duration_sec * 1000);
+          obj.path = response.body.routes[0].geometry;
+
+          resolve(obj);
         });
+      }));
+    }
+
+    function insert(obj) {
+      return highland(new Promise<any>(function(resolve, reject) {
+        total_processed += 1;
+        console.log(total_processed);
+
+        db.none("INSERT INTO trips(start_time, start_point, end_time, end_point, duration_sec, path_polyline) VALUES($1, ST_SetSRID(ST_Point($2, $3),4326), $4, ST_SetSRID(ST_Point($5, $6),4326), $7, $8)", 
+          [obj.start_time, obj.start_lng, obj.start_lat, obj.end_time, obj.end_lng, obj.end_lat, obj.duration_sec, obj.path])
+        .then(function () {
+          resolve(obj);
+        })
+        .catch(function (error) {
+          // error;
+          console.log(error);
+          reject(error);
+        });
+      }));
+    }
+
+    highland(
+      got
+        .stream(url)
+        .pipe(parser)
+    ) // convert to highland stream
+    .drop(1)  // drop the header
+    .batch(2) // 2 records at a time
+    .filter(points_not_same) // make sure the start and end are not the same
+    .take(2000)
+    .map(convert_pair_to_object)
+    .ratelimit(2000, 1000)
+    .flatMap(add_directions)
+    .flatMap(insert)
+    .done(function() {
+      resolve();
     });
   });
 }
@@ -186,14 +148,20 @@ function series(methods) {
 
 let foo = [
   load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-apr14.csv'),
-  load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-may14.csv'),
-  load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-jun14.csv'),
-  load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-jul14.csv'),
-  load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-aug14.csv')
+  // load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-may14.csv'),
+  // load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-jun14.csv'),
+  // load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-jul14.csv'),
+  // load_csv.bind(undefined, 'https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-aug14.csv')
 ];
 
 reset_database()
 .then(function() {
   return series(foo)
 })
-.then(add_index);
+.then(function() {
+  pgp.end();
+});
+
+
+
+
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,19 @@ import * as koa from "koa"
 import serve = require("koa-static");
 import mount = require("koa-mount");
 import * as koa_router from 'koa-router';
-import request = require('request');
 import parse = require('csv-parse');
-import r = require('rethinkdb');
+import * as pgPromise from 'pg-promise';
+
+var pgp = pgPromise();
+
+
+var db = pgp({
+  host: '127.0.0.1', 
+  port: 5432,
+  database: 'uber',
+  user: 'uber',
+  password: 'moo'
+});
 
 const app = new koa();
 const router = new koa_router();
@@ -38,6 +48,97 @@ app.use(mount('/static', serve('../frontend/build')));
 
 router.get('/hello', function *(next) {
   this.body = 'hello';
+});
+
+
+function query_to_linestring(points) {
+  // Coerce to array
+  if (points.constructor === String) {
+    points = [points];
+  }
+
+  // Split lat/long and convert to float
+  points = points.map(function(point) {
+    return point.split(',').map(parseFloat);
+  });
+
+  var linestring = "LINESTRING(";
+  for (var i = 0; i < points.length; i++) {
+    linestring += (points[i][0] +
+      ' ' + points[i][1] + ',');
+  }
+
+  linestring += points[0][0] + ' ' + points[0][1] + ')';
+
+  return linestring;
+}
+
+
+router.get('/containedTrips', function *(next) {
+  var linestring = query_to_linestring(this.request.query.points);
+
+  db.many("SELECT start_point, count(*) as count FROM trips WHERE ST_Within (start_point, ST_Polygon(ST_GeomFromText($1), 4326)) AND ST_Within (end_point, ST_Polygon(ST_GeomFromText($1), 4326)) GROUP BY start_point ORDER BY count DESC LIMIT $2", 
+    [linestring, this.request.query.limit || 10])
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+      console.log("SELECT statement threw error");
+      console.log(error);
+  })
+});
+
+router.get('/commonStart', function *(next) {
+  var linestring = query_to_linestring(this.request.query.points);
+
+  db.many("SELECT ST_AsGeoJSON(start_point), count(*) as count FROM trips WHERE ST_Within (start_point, ST_Polygon(ST_GeomFromText($1), 4326)) GROUP BY start_point ORDER BY count DESC LIMIT $2", 
+    [linestring, this.request.query.limit || 10])
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+      console.log("SELECT statement threw error");
+      console.log(error);
+  })
+});
+
+
+router.get('/trips_in_polygon', function *(next) {
+  var linestring = query_to_linestring(this.request.query.points);
+  var that = this;
+
+  yield db.many("SELECT id, ST_AsGeoJSON(start_point) as start_point, ST_AsGeoJSON(end_point) as end_point, path_polyline FROM trips WHERE ST_Within (start_point, ST_Polygon(ST_GeomFromText($1), 4326)) AND ST_Within (end_point, ST_Polygon(ST_GeomFromText($1), 4326))", 
+    [linestring])
+  .then(function(data) {
+    data = data.map(function(row) {
+      row.start_point = JSON.parse(row.start_point);
+      row.end_point = JSON.parse(row.end_point);
+      return row;
+    })
+    that.response.body = JSON.stringify(data);
+  })
+  .catch(function (error) {
+      console.log("SELECT statement threw error");
+      console.log(error);
+  })
+});
+
+router.get('/all_trips', function *(next) {
+  var that = this;
+
+  yield db.many("SELECT id, ST_AsGeoJSON(start_point) as start_point, ST_AsGeoJSON(end_point) as end_point, path_polyline FROM trips")
+  .then(function(data) {
+    data = data.map(function(row) {
+      row.start_point = JSON.parse(row.start_point);
+      row.end_point = JSON.parse(row.end_point);
+      return row;
+    })
+    that.response.body = JSON.stringify(data);
+  })
+  .catch(function (error) {
+      console.log("SELECT statement threw error");
+      console.log(error);
+  })
 });
 
 app
