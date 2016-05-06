89ab217c9b85d886468798fec43175a7eb73b20e
Craig Younkins <cyounkins@gmail.com>
2016-05-06 12:34:10 -0400

    Remove dead provisioning code for osm2pgrouting

--- provision.sh
+++ provision.sh
@@ -15,8 +15,6 @@ apt-get install -y nodejs \
   python-software-properties git build-essential \
   cmake expat libexpat1-dev libboost-all-dev # Needed to build osm2pgrouting
 
-# apt-get install -y --no-install-recommends osm2pgsql
-
 su -c "psql -c \"CREATE ROLE uber UNENCRYPTED PASSWORD 'moo' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;\"" postgres
 
 PGPASSWORD=moo psql -U uber -h 127.0.0.1 postgres << EOF
@@ -24,29 +22,3 @@ CREATE DATABASE uber;
 \connect uber;
 CREATE EXTENSION postgis;
 EOF
-
-# su -c "psql -c \"CREATE ROLE pgrouting UNENCRYPTED PASSWORD 'moo' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;\"" postgres
-
-# PGPASSWORD=moo psql -U pgrouting -h 127.0.0.1 postgres << EOF
-# CREATE DATABASE 'pgrouting-workshop';
-# \connect pgrouting-workshop;
-# CREATE EXTENSION postgis;
-# CREATE EXTENSION pgrouting;
-# EOF
-
-# wget --quiet http://download.bbbike.org/osm/bbbike/NewYork/NewYork.osm.gz
-# gzip -d NewYork.osm.gz
-
-# PGPASSWORD=moo osm2pgsql -d gis -U uber -H 127.0.0.1 NewYork.osm
-
-# wget --quiet https://github.com/pgRouting/osm2pgrouting/archive/osm2pgrouting-2.1.0.tar.gz
-# tar -zxf osm2pgrouting-osm2pgrouting-2.1.0.tar.gz
-# cd osm2pgrouting-osm2pgrouting-2.1.0
-# cmake -H. -Bbuild
-# cd build/
-# make
-# make install
-# cd ~
-
-# osm2pgrouting --file NewYork.osm --conf /usr/share/osm2pgrouting/mapconfig.xml --dbname pgrouting-workshop --username pgrouting --password moo --host 127.0.0.1 --clean
-
